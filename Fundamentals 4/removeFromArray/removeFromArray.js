80223bbf39b721d2ce07a6a59da189905aa00419
CourseraDSstudent <nikosxanos@gmail.com>
2021-01-02 19:33:23 +0200

    reverseString completed

--- Fundamentals 4/removeFromArray/removeFromArray.js
+++ Fundamentals 4/removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+x = arrayArgs(1, 2, 3, 4);
+const removeFromArray = function arrayArgs() {
+    for (let i = 0; i < arguments.length; i++){
+        sum += arguments[i];
+    }
+    return sum;
 }
 
 module.exports = removeFromArray
