f6c5b5a80cb23790c7601e23524e6b6b4fc0f491
CourseraDSstudent <nikosxanos@gmail.com>
2021-01-04 16:03:35 +0200

    tempConversion completed

--- Fundamentals 4/tempConversion/tempConversion.js
+++ Fundamentals 4/tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function (fahrenheitToConvert) {
+  celsius = (fahrenheitToConvert - 32) * (5 / 9);
+  roundedCelsius = Math.round( celsius * 10) / 10;
+  return roundedCelsius;
 }
 
-const ctof = function() {
-
+const ctof = function (celsiusToConvert) {
+  fahrenheit = ( celsiusToConvert * (9/5) + 32 )
+  roundedFahrenheit = Math.round( fahrenheit * 10 ) / 10;
+  return roundedFahrenheit;
 }
 
 module.exports = {
