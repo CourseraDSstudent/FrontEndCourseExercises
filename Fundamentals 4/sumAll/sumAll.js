d03b42199a472798f6a47524e76afe7aa3c95ae7
CourseraDSstudent <nikosxanos@gmail.com>
2021-01-05 23:05:00 +0200

    sumAll 5/6 tests passed

--- Fundamentals 4/sumAll/sumAll.js
+++ Fundamentals 4/sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let min = Math.min(num1, num2);
+    let max = Math.max(num1, num2);
+    let total = 0;
+    if (min > 0 && (typeof min === 'number') && (typeof max === 'number')) {
+    for(let i = min; i <= max; i++) {
+        total += i;
+    }
+        return total;
+    }
+    else
+        return 'ERROR';
 }
 
 module.exports = sumAll
