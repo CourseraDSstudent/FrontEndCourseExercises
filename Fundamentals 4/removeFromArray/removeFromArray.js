b8460c78157d6f9119a2d86d6e04dd387a75dd6f
CourseraDSstudent <nikosxanos@gmail.com>
2021-01-16 20:12:21 +0200

    small changes

--- Fundamentals 4/removeFromArray/removeFromArray.js
+++ Fundamentals 4/removeFromArray/removeFromArray.js
@@ -1,9 +1,15 @@
-x = arrayArgs(1, 2, 3, 4);
-const removeFromArray = function arrayArgs() {
-    for (let i = 0; i < arguments.length; i++){
-        sum += arguments[i];
+const removeFromArray = function arrayArgs(arr,...rest) {
+    for (let i = 0; i < arr.length; i++){
+        itemToRemove = rest;
+        // debugger;
+        // if ( arr[i] === 3) {
+        // arr.splice(i, 1);
+        // }
+        /* else */ if (arr[i] = typeof(String)) {
+            arr.splice(i, 1);
+        }
     }
-    return sum;
+    return arr;
 }
 
 module.exports = removeFromArray
